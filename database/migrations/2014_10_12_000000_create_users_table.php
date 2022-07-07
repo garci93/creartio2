5045b5d869f85d5ccd8288b4289b0b8cad3ff970
José Joaquín Garci Peña <josejoaquin.garci@iesdonana.org>
2022-07-07 17:23:48 +0200

    Purgando base de datos

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,14 +15,43 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nombre');
             $table->string('email')->unique();
+            $table->string('rol');
+            $table->string('nom_completo');
+            $table->string('biografia')->nullable();
+            $table->string('idioma')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
     }
+
+    REATE TABLE users (
+        id            bigserial       PRIMARY KEY
+      , nombre        VARCHAR(255)    NOT NULL UNIQUE
+      , email         VARCHAR(255)    NOT NULL
+      , rol           VARCHAR(255)    NOT NULL
+      , nom_completo  VARCHAR(255)    NOT NULL
+      , biografia     VARCHAR(255)
+      , idioma        VARCHAR(255)
+      , auth_key      VARCHAR(255)
+      , password      VARCHAR(255)    NOT NULL
+  
+  
+      $table->id();
+              $table->string('nombre');
+              $table->string('apellidos');
+              $table->string('nickname');
+              $table->string('email')->unique();
+              $table->timestamp('email_verified_at')->nullable();
+              $table->string('password');
+              $table->rememberToken();
+              $table->foreignId('current_team_id')->nullable();
+              $table->string('profile_photo_path', 2048)->nullable();
+              $table->timestamps();
+  );
 
     /**
      * Reverse the migrations.
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +23,6 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Route::resource('Post', PostController::class);
+
